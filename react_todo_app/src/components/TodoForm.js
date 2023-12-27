c9729e6651ebd2fae2639814a9c376f71291962c
Mukul Kumar <mkunboxing@gmail.com>
2023-12-27 10:37:09 +0530

    bug fixed

--- react_todo_app/src/components/TodoForm.js
+++ react_todo_app/src/components/TodoForm.js
@@ -1,20 +1,35 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-export default function TodoForm({addTodo}) {
-    const [value, setValue] = useState("");
+export default function TodoForm({ addTodo }) {
+  const [value, setValue] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        addTodo(value);
-
-        setValue("");
+    // Check if the value is not empty before adding the todo
+    if (value.trim() !== '') {
+      addTodo(value);
+      setValue('');
+    } else {
+      // Handle empty input case (e.g., show an error message)
+      // For now, just log an error message to the console
+      console.error('Input cannot be empty!');
     }
+  };
 
   return (
-    <form className='TodoForm' onSubmit={handleSubmit}>
-        <input type="text" className="todo-input"value={value} placeholder='what is the task today' onChange={(e) => setValue(e.target.value)}/>
-        <button type="submit" className="todo-btn">Add Task</button>
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="todo-input"
+        value={value}
+        placeholder="What is the task today"
+        required
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button type="submit" className="todo-btn">
+        Add Task
+      </button>
     </form>
-  )
+  );
 }
